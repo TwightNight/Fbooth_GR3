0469c72940e87d8baa4173fb833ec82ef7884682
Nguyễn Duy <duynt0319@gmail.com>
2024-06-14 02:14:44 +0700

    Init commit

---
+++ PhotoboothWpf/Classes/EmailSender.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Xml;
+using System.Xml.Linq;
+using EOSDigital.SDK;
+
+
+namespace PhotoboothWpf.Classes
+{
+    class EmailSender
+    {
+        private string smtpServerName;
+        private string smtpPortNumber;
+        private string emailHostAddress;
+        private string emailHostPassword;
+
+        private XDocument settings = new XDocument();
+        private string currentDirectory = Environment.CurrentDirectory;
+
+        public async void SendEmail(int photoNumber, int numberOfPhotosToSendViaEmail, string emailClientAddress)
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+
+                    LoadValues();
+                    MailMessage mail = new MailMessage();
+                    //put your SMTP address and port here.
+                    SmtpClient SmtpServer = new SmtpClient(smtpServerName);
+                    //Put the email address
+                    mail.From = new MailAddress(emailHostAddress);
+                    //Put the email where you want to send.
+                    //TODO: Regexp sprawdzajacy poprawny e-mail
+                    mail.To.Add(emailClientAddress);
+
+                    mail.Subject = "Test1234Topic";
+
+                    StringBuilder sbBody = new StringBuilder();
+
+                    sbBody.AppendLine("Hi, this is test mail");
+
+                    mail.Body = sbBody.ToString();
+
+                    var instance = new SavePhoto(photoNumber);
+                    for (int i = 0; i < numberOfPhotosToSendViaEmail; i++)
+                    {
+                        photoNumber = (instance.PhotoNumberJustTaken() - i);
+                        Debug.WriteLine("photo number is: " + photoNumber);
+                        string photoName = instance.photoNaming(photoNumber);
+                        string photoDirectoryPath = Path.Combine(Actual.FilePath(), photoName);
+                        Debug.WriteLine(photoDirectoryPath);
+                        Attachment attachment = new Attachment(photoDirectoryPath);
+                        mail.Attachments.Add(attachment);
+                    }
+
+                    /// mail - photomadnesstest@hotmail.com
+                    /// pass - Photomadness123
+                    /// Server name: smtp-mail.outlook.com
+                    //Port: 587
+                    //TODO:dodac mozliwosc podmiany maili
+                    SmtpServer.Credentials = new NetworkCredential(emailHostAddress, emailHostPassword);
+                    SmtpServer.Port = int.Parse(smtpPortNumber);
+                    SmtpServer.EnableSsl = true;
+
+                    SmtpServer.Send(mail);
+                });
+            }
+            catch (FormatException) { Report.Error("Wrong e-mail format \nPlease enter your e-mail correctly\nexample@mail.com", true); }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+        }
+
+        public void LoadValues()
+        {
+            try
+            {
+                settings = XDocument.Load(Path.Combine(currentDirectory, "menusettings.xml"));
+                settings.Root.Elements("setting");
+                emailHostAddress = settings.Root.Element("EmailHostAddress").Value;
+                emailHostPassword = settings.Root.Element("EmailHostPassword").Value;
+                smtpServerName = settings.Root.Element("SmtpServerName").Value;
+                smtpPortNumber = settings.Root.Element("SmtpPortNumber").Value;
+             }
+            catch (XmlException e)
+            {
+                Debug.WriteLine("LoadDefaultValues exception");
+            }
+            catch (NullReferenceException e)
+            {
+                Debug.WriteLine("missing settings in menusettings.xml");
+            }
+
+
+
+        }
+    }
+}
+
---
+++ PhotoboothWpf/Menu.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+using Path = System.IO.Path;
+
+namespace PhotoboothWpf
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+   
+    public partial class Menu : Page
+    {
+        List<string> foregroundList = new List<string>();
+        List<string> printerList = new List<string>();
+        List<int> copiesCount = new List<int>();
+
+        UserSettings userSettings = new UserSettings();
+        XDocument settings = new XDocument();
+        XDocument userSettingXDoc = new XDocument();
+        private string currentDirectory = Environment.CurrentDirectory;
+        List<string> variables = new List<string>();
+
+
+        public Menu()
+        {
+           // FillDefaultValue();
+            InitializeComponent();
+            FillList();
+            FillComboBox();
+        }
+        public void FillList()
+        {
+            foregroundList.Add("foreground_1");
+            foregroundList.Add("foreground_3");
+            foregroundList.Add("foreground_4");
+            foregroundList.Add("foreground_4_paski");
+            foregroundList.Add("All");
+
+            variables.Add("WelcomeText");
+            variables.Add("BeforePhotoText");
+            variables.Add("FirstPhotoText");
+            variables.Add("SecondPhotoText");
+            variables.Add("ThirdPhotoText");
+            variables.Add("FourthPhotoText");
+            variables.Add("backgroundPath");
+            variables.Add("buttonsColor");
+            variables.Add("borderColor");
+            variables.Add("textBoxColor");
+            variables.Add("buttonHighlightColor");
+
+            foreach (string printer in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                printerList.Add(printer);
+            }
+
+            for(int i=1;i<6;i++)
+            {
+                copiesCount.Add(i);
+            }
+        }
+        public void FillComboBox()
+        {
+            ForegroundComboBox.ItemsSource = foregroundList;
+            PrinterComboBox.ItemsSource = printerList;
+            Printer2ComboBox.ItemsSource = printerList;
+            CopiesComboBox.ItemsSource = copiesCount;
+            SettingsComboBox.ItemsSource = variables;
+            LoadDefaultValues();
+
+        }
+        public void LoadDefaultValues()
+        {
+            try
+            {
+                settings = XDocument.Load(Path.Combine(currentDirectory, "menusettings.xml"));
+                settings.Root.Elements("setting");
+                ForegroundComboBox.SelectedValue = settings.Root.Element("actualTemplate").Value;
+                PrinterComboBox.SelectedValue = settings.Root.Element("actualPrinter").Value;
+                Printer2ComboBox.SelectedValue = settings.Root.Element("secondPrinter").Value;
+                CopiesComboBox.SelectedValue = Convert.ToInt32(settings.Root.Element("maxNumberOfCopies").Value);
+                TimeBetweenPhotosSlider.Value = Convert.ToDouble(settings.Root.Element("timeBetweenPhotos").Value);
+                PrintingTimeSlider.Value = Convert.ToDouble(settings.Root.Element("printingTime").Value);
+                ChangeEmailAddressTextBox.Text = settings.Root.Element("EmailHostAddress").Value;
+                ChangeEmailPasswordTextBox.Text = settings.Root.Element("EmailHostPassword").Value;
+                ChangeEmailServerTextBox.Text = settings.Root.Element("SmtpServerName").Value;
+                ChangeEmailPortTextBox.Text = settings.Root.Element("SmtpPortNumber").Value;
+            }
+            catch (XmlException e)
+            { Debug.WriteLine("LoadDefaultValues exception");}
+            catch(NullReferenceException e) { Debug.WriteLine("missing settings in menusettings.xml");}
+            
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            Save();
+            Application.Current.Shutdown();
+        }
+
+        private void Save()
+        {
+            try
+            {
+                var savedata = new MenuData();
+                savedata.FillValues(ForegroundComboBox.SelectedValue.ToString(),
+                                    PrinterComboBox.SelectedValue.ToString(),
+                                    TimeBetweenPhotosSlider.Value.ToString(),
+                                    PrintingTimeSlider.Value.ToString(),
+                                    CopiesComboBox.SelectedValue.ToString(),
+                                    Printer2ComboBox.SelectedValue.ToString(),
+                                    ChangeEmailAddressTextBox.Text,
+                                    ChangeEmailPasswordTextBox.Text,
+                                    ChangeEmailServerTextBox.Text,
+                                    ChangeEmailPortTextBox.Text);
+            }
+            catch (Exception e)
+            {
+                // TODO: Update which exception
+                Console.WriteLine(e);
+            }
+            
+
+        }
+
+        private void WithoutSaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ChangeTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = SettingsComboBox.SelectedValue.ToString();
+            string value = SettingsTextBox.Text;
+            userSettings.ChangeText(name, value);
+        }
+
+        private void SettingsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            userSettingXDoc = XDocument.Load(Path.Combine(currentDirectory, "UserSettings.xml"));
+            userSettingXDoc.Root.Elements("FrontEnd");
+            string actualValue = SettingsComboBox.SelectedValue.ToString();
+            string var = userSettingXDoc.Root.Element(actualValue).Value;
+            SettingsTextBox.Text = var;
+        }
+        private void FillDefaultValue()
+        {            
+            userSettings.SetNewUserSettings("Hello", "Prepare for first photo", "Get ready for second one", "Third photo coming, prepare!", "the end",
+               "backgrounds", "backgrounds", "red", "orange", "orange", "blue");
+            userSettings.SaveOptions("ble");
+        }
+    }
+}
