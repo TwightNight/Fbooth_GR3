9e17419be5a745d0059ac55c8e77895ac32588db
minhduc1706 <minhduc0879395@gmail.com>
2024-11-05 22:37:14 +0700

    design

--- FBoothApp/obj/Debug/FBoothApp_Content.g.i.cs
+++ FBoothApp/obj/Debug/FBoothApp_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("backgrounds/tecday.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("backgrounds/retake.png")]
 
 
