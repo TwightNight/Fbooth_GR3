6968b3eddddeba4d7382e95463da91dc7fdf8035
minhduc1706 <minhduc0879395@gmail.com>
2024-11-08 10:26:32 +0700

    design start screen

--- FBoothApp/obj/Debug/FBoothApp_Content.g.i.cs
+++ FBoothApp/obj/Debug/FBoothApp_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("backgrounds/letter.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("backgrounds/techday1.jpg")]
 
 
